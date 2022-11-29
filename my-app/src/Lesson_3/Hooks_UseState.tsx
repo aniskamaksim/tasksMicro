e302ef04b0413b3a2723a8328768ce40cb07f7fa
Maksim Aniska <aniskamaksim@gmail.com>
2022-11-29 10:06:11 +0300

    feat: add lesson 3

---
+++ my-app/src/Lesson_3/Hooks_UseState.tsx
@@ -0,0 +1,29 @@
+import React, {useState} from 'react';
+import "./HooksUseState.css";
+/*
+let a = 1;
+const onclickHandler = () => {
+       ++a;
+        console.log(a)
+    }
+ в консоли значение меняется, но единица остается единицей
+*/
+
+export const HooksUseState = () => {
+    //let a = 1;
+    let[a, setA] = useState(1)
+    const onclickHandler = () => {
+       setA(++a);
+        console.log(a)
+    }
+    const reset = () => {
+        setA(1);
+    }
+    return (
+        <div className={'example'}>
+            <div><h1>{a}</h1></div>
+            <div><button onClick={onclickHandler}>number</button></div>
+            <div><button onClick={reset}>reset</button></div>
+        </div>
+    );
+};
