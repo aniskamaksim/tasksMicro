decd17d6f2b466f65cd8cbf32e208cc3d5c17c13
Maksim Aniska <aniskamaksim@gmail.com>
2022-11-30 20:06:56 +0300

    feat: add 4 lesson filter method

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {HooksUseState} from "./Lesson_3(_useState_hooks)/Hooks_UseState";
+import {Filter_method} from "./Lesson_4(_filter_)/filter_method"
 
 function App() {
   return (
     <div>
-      <HooksUseState />
+        {/*<HooksUseState/>*/}
+        <Filter_method/>
     </div>
   );
 }
--- my-app/src/Lesson_4(_filter_)/filter_method.tsx
+++ my-app/src/Lesson_4(_filter_)/filter_method.tsx
@@ -1,6 +1,6 @@
 import React, {useState} from "react";
 
-function App() {
+export function Filter_method() {
     const [money, setMoney] = useState([
         {banknote: 'dollar', nominal: 100, number: 'a1234567890'},
         {banknote: 'dollar', nominal: 50, number: 'z1234567890'},
@@ -11,17 +11,44 @@ function App() {
         {banknote: 'dollar', nominal: 50, number: 'y1234567890'},
         {banknote: 'rouble', nominal: 50, number: 'w1234567890'}
     ])
+    {/*вешаем хук, передаем в константу Filter значение nameButton, в onClickHandlerе сетаем значение*/}
+    const [Filter, setFilter] = useState('all')
+
+    let currentMoney = money;
+    if (Filter === 'rouble') {
+        currentMoney = money.filter((e) => e.banknote === 'rouble')
+    } else if (Filter === 'dollar') {
+        currentMoney = money.filter((e) => e.banknote === 'dollar')
+    } else {
+        currentMoney = money;
+    }
+
+
+    const onClickFilterHandler = (nameButton: string) => {
+       setFilter(nameButton)
+
+    }
     return (
-        <ul>
-            {money.map((objFromMoneyArr)=>{
-                return(
-                    <li>
-                        <span>{objFromMoneyArr.banknote}</span>
-                        <span>{objFromMoneyArr.nominal}</span>
-                        <span>{objFromMoneyArr.number}</span>
-                    </li>
-                )
-            })}
-        </ul>
+        <div>
+            <ul>
+                {/*objFromMoneyArr он же е он же el он же element он же Жора он же Гога*/}
+                {currentMoney.map((objFromMoneyArr, index)=>{
+                    return(
+                        <div key={index}>
+                        <li>
+                            <span>{objFromMoneyArr.banknote}</span>
+                            <span>{objFromMoneyArr.nominal}</span>
+                            <span>{objFromMoneyArr.number}</span>
+                        </li>
+                        </div>
+                          )
+                })}
+            </ul>
+            <div>
+                <button onClick={()=>onClickFilterHandler('all')}>All</button>
+                <button onClick={()=>onClickFilterHandler('dollar')}>Dollar</button>
+                <button onClick={()=>onClickFilterHandler('rouble')}>Rouble</button>
+            </div>
+        </div>
     )
 }
